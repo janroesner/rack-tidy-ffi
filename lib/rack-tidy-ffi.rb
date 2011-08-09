f2e3c8c8f32faa6ba66e5353b19af28110c8373f
Jan Roesner <jan@roesner.it>
2011-08-09 11:37:26 +0200

    Changed @response.first to @response.body and deleted the HTML5 header.

--- lib/rack-tidy-ffi.rb
+++ lib/rack-tidy-ffi.rb
@@ -3,23 +3,21 @@ require "#{path}/tidy-machine"
 
 class RackTidyFFI
   attr_accessor :headers
-  
+
   def initialize(app)
-    @app = app       
-  end             
-  
-  def call(env)   
+    @app = app
+  end
+
+  def call(env)
     @status, @headers, @response = @app.call(env)
     # TODO: include all response attributes... needed?
     # response_body = ""
     # response_body = @response.each { |part| response_body += part }
     if @headers["Content-Type"].include? "text/html"
-      response = TidyMachine.new(@response.first).tidy
-      response = "<!DOCTYPE HTML>\n#{response}"
+      response = TidyMachine.new(@response.body).tidy
       @headers["Content-Length"] = response.length.to_s
       @response = [response]
     end
     [@status, @headers, @response]
-  end            
-  
+  end
 end
